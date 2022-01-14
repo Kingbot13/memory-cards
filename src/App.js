2fa40f50de905340d4aa5d92d1e7465d079048c5
Dylan King <dwkblue51@yahoo.com>
2022-01-14 08:22:54 -0600

    setup initial markup structure in App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import './App.css';
+import Scoreboard from './components/Scoreboard';
+import Cards from './components/Cards';
 
 function App() {
   return (
-    
+    <div>
+      <header>
+        <h1>Marvel Memory Cards</h1>
+      </header>
+      <main>
+        <Scoreboard />
+        <Cards />
+      </main>
+      <footer>
+        <p>Created by Dylan King</p>
+      </footer>
+    </div>
   );
 }
 
